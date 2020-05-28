586bddb843a60802b6fcb218f02e85c012374c83
kiarash97 <kasirikiarash@gmail.com>
2020-05-28 15:50:36 -0600

    state transition model

--- Test/Networks/ModelNN/StateTransitionModel.py
+++ Test/Networks/ModelNN/StateTransitionModel.py
@@ -5,27 +5,44 @@ import torch.nn.functional as F
 
 
 class StateTransitionModel(nn.Module):
-    def __init__(self, state, action):
+    def __init__(self, state_shape, layers_type, layers_features):
         super(StateTransitionModel, self).__init__()
-        self.state_shape = state.shape
-        linear_states_shape = 1
-        action_shape = 1
-        for shape in state.shape:
-            print(shape)
-            linear_states_shape *= shape
-        for shape in action.shape:
-            action_shape *= shape
 
-        nh1 = 50
-        nh2 = 50
-        self.fc1 = nn.Linear(linear_states_shape + action_shape, nh1)
-        self.fc2 = nn.Linear(nh1, nh2)
-        self.head = nn.Linear(nh2, linear_states_shape)
+        # state : Batch, W, H, Channels
+        # action: Batch, A
+        self.layers_type = layers_type
+        self.layers = []
+        linear_input_size = 0
+        for i, layer in enumerate(layers_type):
+            if layer == 'conv':
+                raise NotImplemented("convolutional layer is not implemented")
+            elif layer == 'fc':
+                if i == 0:
+                    linear_input_size = state_shape[1] * state_shape[2] * state_shape[3]
+                    self.layers.append(nn.Linear(linear_input_size, layers_features[i]))
+                else:
+                    self.layers.append(nn.Linear(layers_features[i - 1], layers_features[i]))
+            else:
+                raise ValueError("layer is not defined")
 
-    def forward(self, prev_state, prev_action):
-        x = prev_state.flatten(start_dim= 1)
-        x = torch.cat((x, prev_action),dim= 1)
-        x = self.fc1(x)
-        x = self.fc2(x)
-        x = self.head(x)
-        return x.view((-1,) + self.state_shape) # -1 is for the batch size
+        self.head = nn.Linear(layers_features[-1], linear_input_size)
+
+
+    def forward(self, state):
+        x = 0
+        for i, layer in enumerate(self.layers_type):
+            if layer == 'conv':
+                raise NotImplemented("convolutional layer is not implemented")
+            elif layer == 'fc':
+                if i == 0:
+                    s = state.flatten(start_dim= 1)
+                    x = s
+                    x = self.layers[i](x.float())
+                else:
+                    x = self.layers[i](x.float())
+
+            else:
+                raise ValueError("layer is not defined")
+        x = self.head(x.float())
+        return x.view((-1,) + state.shape) # -1 is for the batch size
+
