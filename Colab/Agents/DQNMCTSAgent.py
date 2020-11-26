d2c1c08dffa9c9e37017bab2968f69a6475e40fe
kohankhaki <kohankha@ualberta.ca>
2020-11-26 14:56:13 -0700

    Learn from selection and learn from rollout added.

--- Colab/Agents/DQNMCTSAgent.py
+++ Colab/Agents/DQNMCTSAgent.py
@@ -1,29 +1,11 @@
 import numpy as np
+import torch
 import random
-from torch.utils.tensorboard import SummaryWriter
-from ete3 import Tree, TreeStyle, TextFace, add_face_to_node
+import time
 
 import Colab.utils as utils
 from Colab.Agents.BaseDynaAgent import BaseDynaAgent
 from Colab.Agents.MCTSAgent import BaseMCTSAgent
-
-
-import copy
-import numpy as np
-import torch
-from torch.utils.tensorboard import SummaryWriter
-import torch.nn as nn
-import torch.nn.functional as F
-
-import torch.optim as optim
-from abc import abstractmethod
-import random
-
-import Colab.utils as utils
-from Colab.Agents.BaseAgent import BaseAgent
-from Colab.Networks.ValueFunctionNN.StateActionValueFunction import StateActionVFNN3, StateActionVFNN4
-from Colab.Networks.ValueFunctionNN.StateValueFunction import StateVFNN
-from Colab.Networks.RepresentationNN.StateRepresentation import StateRepresentation
 
 
 class DQNMCTSAgent(BaseMCTSAgent, BaseDynaAgent):
@@ -33,6 +15,9 @@ class DQNMCTSAgent(BaseMCTSAgent, BaseDynaAgent):
         BaseMCTSAgent.__init__(self, params)
         BaseDynaAgent.__init__(self, params)
         self.action_consistency = 0
+        self.learn_from_roullout = False
+        self.learn_from_selection = False
+        self.learn_from_expansion = True
 
     def start(self, observation):
         self.action_consistency = 0
@@ -78,24 +63,48 @@ class DQNMCTSAgent(BaseMCTSAgent, BaseDynaAgent):
                 transition_batch = self.getTransitionFromBuffer(n=self._vf['q']['batch_size'])
                 self.updateValueFunction(transition_batch, 's')
 
+    def selection(self):
+        time1 = time.time()
+        self.timecounter[2] += 1
+        selected_node = self.root_node
+        while len(selected_node.get_childs()) > 0:
+            max_uct_value = -np.inf
+            child_values = list(map(lambda n: n.get_avg_value(), selected_node.get_childs()))
+            max_child_value = max(child_values)
+            min_child_value = min(child_values)
+            for child in selected_node.get_childs():
+                if child.num_visits > 0:
+                    child_value = child.get_avg_value()
+                    if min_child_value != np.inf and max_child_value != np.inf and min_child_value != max_child_value:
+                        child_value = (child.get_avg_value() - min_child_value) / (max_child_value - min_child_value)
+                    uct_value = child_value + self.C * ((selected_node.num_visits / child.num_visits)**0.5)
+                else:
+                    uct_value = np.inf
+                if max_uct_value < uct_value:
+                    max_uct_value = uct_value
+                    selected_node = child
+            self.timelist[2] += time.time() - time1
+
+            if self.learn_from_selection:
+                state = selected_node.parent.get_state()
+                action = selected_node.action_from_par
+                reward = selected_node.reward_from_par
+                next_state = selected_node.get_state()
+                is_terminal = selected_node.is_terminal
+                self.learn_from_transition(state, action, reward, next_state, is_terminal)
+
+        return selected_node
+
     def expansion(self, node):
         for a in self.action_list:
             state = node.get_state()
             next_state, is_terminal, reward = self.true_model(state, a)  # with the assumption of deterministic model
-            torch_state = self.getStateRepresentation(state)
-            torch_next_state = self.getStateRepresentation(next_state)
-            torch_reward = torch.tensor([reward], device=self.device)
-            torch_action = torch.tensor([self.getActionIndex(a)], device=self.device).view(1,1)
-            transition = utils.transition(torch_state,
-                             torch_action,
-                             torch_reward,
-                             torch_next_state,
-                             None, is_terminal, self.time_step, 0)
-            self.updateTransitionBuffer(transition)
-            if np.array_equal(next_state, state):
+            if np.array_equal(next_state, state):  # To be deleted later - Gridworld related
                 continue
             child = Node(node, next_state, is_terminal=is_terminal, action_from_par=a, reward_from_par=reward)
             node.add_child(child)
+            if self.learn_from_expansion:
+                self.learn_from_transition(state, a, reward, next_state, is_terminal)
 
     def rollout(self, node):
         is_terminal = False
@@ -108,8 +117,23 @@ class DQNMCTSAgent(BaseMCTSAgent, BaseDynaAgent):
                 next_state, is_terminal, reward = self.true_model(state, a)
                 returns[i] += reward
                 depth += 1
+                if self.learn_from_roullout:
+                    self.learn_from_transition(state, a, reward, next_state, is_terminal)
                 state = next_state
         return np.average(returns)
+
+    def learn_from_transition(self, state, a, reward, next_state, is_terminal):
+        torch_state = self.getStateRepresentation(state)
+        torch_next_state = self.getStateRepresentation(next_state)
+        torch_reward = torch.tensor([reward], device=self.device)
+        torch_action = torch.tensor([self.getActionIndex(a)], device=self.device).view(1, 1)
+        transition = utils.transition(torch_state,
+                                      torch_action,
+                                      torch_reward,
+                                      torch_next_state,
+                                      None, is_terminal, self.time_step, 0)
+        self.updateTransitionBuffer(transition)
+
 
 class Node():
     def __init__(self, parent, state, value=0, is_terminal=False, action_from_par=None, reward_from_par=0):
